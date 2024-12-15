d1f6cfe80015af46d6971ebc21a4f5d4215c1a12
Michal Maruska <mmaruska@gmail.com>
2024-12-15 22:53:34 +0100

    rebuild-packages.sh: add comments.

--- rebuild-packages.sh
+++ rebuild-packages.sh
@@ -7,7 +7,7 @@
 
 set -x
 
-# What's the status of dists? Which one is better?
+# What's the status of `dists'? Which one is better?
 mkdir --parents ok
 if [[ ! -d ok/dists ]]
 then
@@ -15,33 +15,41 @@ then
 fi
 
 
-# db: we don't need it:
+# `db': we don't need it:
 mkdir --parents db.backup
+# what if it's already there:
 mv -v db/packages.db db.backup/ || : ignore
 
 
 # regenerate: from the dists
 rm -fv db/packages.db
-# this is a secondary index:
+
+# this is a `secondary' index:
 # rm db/packagenames.db
 mv -v db/packagenames.db db.backup/ || : ignore
 
 
+#
 rm -vf db/references.db
 
 # ===========================
+# here we need `localreadd' ./conf/distributions
+# and ./conf/updates
 reprepro update sid
 # populates lists/localreadd_sid_*
 reprepro export
 # ===========================
 
+
 # fixme: others!
 reprepro dumpunreferenced
-
+#
 reprepro rereference
 
 rm -fv db/checksums.db
 reprepro collectnewchecksums
 
-
+#
 reprepro dumpunreferenced
+reprepro checkpool
+reprepro check
