057605779754d3227dd4e84fcafe4839570412a9
Michal Maruska <mmaruska@gmail.com>
2024-12-15 02:05:28 +0100

    improve more

--- rebuild-packages.sh
+++ rebuild-packages.sh
@@ -5,19 +5,31 @@
 # This file contains multiple databases, one for each target, containing
 # the chunks from the Packages or Sources files, indexed by package name.
 
-mv dists dists.ok
+set -x
+mkdir --parents ok
+mv -vf dists ok/dists || : ignore
 
+mkdir --parents db.backup
 
-# rm db/packages.db
+mv -v db/packages.db db.backup/ || : ignore
+rm -fv db/packages.db
 # this is a secondary index:
 # rm db/packagenames.db
+mv -v db/packagenames.db db.backup/ || : ignore
+
+
+rm -vf db/references.db
+
 reprepro update sid
+# fixme: others!
 
 #
 reprepro dumpunreferenced
 
-# rm db/references.db
 reprepro rereference
 
-# rm db/checksums.db
+rm -fv db/checksums.db
 reprepro collectnewchecksums
+
+
+reprepro dumpunreferenced
