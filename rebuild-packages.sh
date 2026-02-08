04ee50412777507d8f6b2735af86da5d2ad42c4a
Michal Maruska <mmaruska@gmail.com>
2026-02-08 15:23:22 +0100

    format

--- rebuild-packages.sh
+++ rebuild-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh -feu
 
 # https://github.com/ionos-cloud/reprepro/blob/multiple-versions-debian/docs/recovery
-# packages.db
+# `packages.db'
 # This file contains multiple databases, one for each target, containing
 # the chunks from the Packages or Sources files, indexed by package name.
 
